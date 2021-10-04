e7c8574a9deaaa265e621a81c88470d26213af6c
daniel <Daniel.serna.olarte@gmail.com>
2021-10-04 12:30:44 -0500

    cambiando argumentos del modelo

--- TTCBin.py
+++ TTCBin.py
@@ -13,6 +13,8 @@ import logging
 from sklearn.model_selection import train_test_split
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 
+from datetime import datetime
+
 #loggers basic configurations
 logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
@@ -24,21 +26,36 @@ train_df, eval_df = train_test_split(toxTwentyCastdf, test_size=0.3)
 
 # Optional model configuration
 EPOCHS = 100
-seyonec = 'seyonec/BPE_SELFIES_PubChem_shard00_120k'
+seyonec120K = 'seyonec/BPE_SELFIES_PubChem_shard00_120k'
+seyonec150K = 'seyonec/BPE_SELFIES_PubChem_shard00_150k'
+seyonec160K = 'seyonec/BPE_SELFIES_PubChem_shard00_160k'
+
+
+#data and time for today
+now = datetime.now()
+todayDir = now.strftime("outputs/%d-%m-%Y-%H%M/")
+todayBestDir = now.strftime("outputs/%d-%m-%Y-%H%M/best_model")
 
 # Create a ClassificationModel
-model_args = ClassificationArgs(
-    num_train_epochs = EPOCHS,
-    wandb_project = 'toxTwentyCast',
-    tokenizer_name=seyonec)
+model_args = ClassificationArgs()
+model_args.num_train_epochs = EPOCHS
+model_args.wandb_project = 'toxTwentyCast'
+model_args.tokenizer_name=seyonec120K
+model_args.save_model_every_epoch=False
+model_args.evaluate_during_training = True
+model_args.output_dir = todayDir
+model_args.best_model_dir = todayBestDir
+model_args.eval_batch_size = 128
+model_args.train_batch_size = 128
+model_args.use_early_stopping = True
 
-# Create a ClassificationModel
+# Create a ClassificationModel9
 model = ClassificationModel(
-    "roberta", seyonec, args=model_args,
+    "roberta", seyonec120K, args=model_args,
 )
 
 # Train the model
-model.train_model(train_df)
+model.train_model(train_df, eval_data=eval_df)
 
 # Evaluate the model
 result, model_outputs, wrong_predictions = model.eval_model(eval_df)
