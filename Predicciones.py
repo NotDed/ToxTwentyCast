a53b02fe894f62001ce5fcd1488ce7a17a7822eb
daniel <Daniel.serna.olarte@gmail.com>
2021-10-07 12:26:26 -0500

    Predicciones con wandb

--- Predicciones.py
+++ Predicciones.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 import torch
 
-from simpletransformers.classification import ClassificationModel
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
 
 
 # Optional model configuration
@@ -10,8 +10,12 @@ mol = sys.argv[1]
 
 save = sys.argv[2]
 
+# Create a ClassificationModel
+model_args = ClassificationArgs()
+model_args.wandb_project = 'toxTwentyCastBin'
+
 model = ClassificationModel(
-    "roberta", save
+    "roberta", save, args=model_args,
 )
 
 predictions, raw_outputs = model.predict([mol])
