630460258921fb98e25ac097fcb9350b95d0a0ba
daniel <Daniel.serna.olarte@gmail.com>
2021-10-07 11:37:52 -0500

    Modelo para predicciones

---
+++ Predicciones.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sys
+import torch
+
+from simpletransformers.classification import ClassificationModel
+
+
+# Optional model configuration
+mol = sys.argv[1]
+
+save = sys.argv[2]
+
+model = ClassificationModel(
+    "roberta", , args=model_args,
+)
+
+predictions, raw_outputs = model.predict([mol])
+
+print(predictions, raw_outputs)
