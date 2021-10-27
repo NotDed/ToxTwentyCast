26aef4ba911d3d9d0a53a029a6d2c2e17d7d1424
daniel <Daniel.serna.olarte@gmail.com>
2021-10-27 16:36:40 -0500

    roc_auc

--- TTCBin.py
+++ TTCBin.py
@@ -71,7 +71,7 @@ model_args.best_model_dir = todayBestDir
 
 model_args.reprocess_input_data = True
 model_args.overwrite_output_dir = True
-model_args.max_seq_length = 128
+# model_args.max_seq_length = 128
 model_args.num_train_epochs = epoch
 model_args.train_batch_size = 16
 model_args.eval_batch_size = 16
@@ -83,11 +83,19 @@ model_args.use_cached_eval_features = True
 
 model_args.use_early_stopping = True
 model_args.early_stopping_delta = 0.01
-model_args.early_stopping_metric = "eval_loss"
+model_args.early_stopping_metric = "roc_auc_score"
 model_args.early_stopping_metric_minimize = True
 model_args.early_stopping_patience = 3
 
-model_args.config = {"dropout": 0.4}
+model_args.learning_rate = 1e-4
+model_args.max_seq_length = 512
+
+
+model_args.adafactor_eps = 1e-12
+model_args.config = {
+        "dropout": 0.1,
+        "layer_norm_eps": 1e-12
+    }
 
 model_args.wandb_project = 'toxTwentyCastBin'
 
@@ -97,10 +105,10 @@ model = ClassificationModel(
 )
 
 # Train the model
-model.train_model(train_df, eval_df=val_df, acc=metrics.accuracy_score, aps=metrics.average_precision_score)
+model.train_model(train_df, eval_df=val_df, acc=metrics.accuracy_score, aps=metrics.average_precision_score, roc = metrics.roc_auc_score)
 
 # accuracy
-result, model_outputs, wrong_predictions = model.eval_model(val_df, acc=metrics.accuracy_score)
+# result, model_outputs, wrong_predictions = model.eval_model(val_df, acc=metrics.accuracy_score)
 
 # ROC-PRC
-result, model_outputs, wrong_predictions = model.eval_model(val_df, aps=metrics.average_precision_score)
+# result, model_outputs, wrong_predictions = model.eval_model(val_df, aps=metrics.average_precision_score)
