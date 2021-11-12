05d8f89b03b8c26c222cb137b3c64caab12d9d24
daniel <Daniel.serna.olarte@gmail.com>
2021-11-11 22:44:26 -0500

    TTCBin con TorchText

---
+++ TTCBin2.py
@@ -0,0 +1,130 @@
+#pip install -U torch==1.8.0 torchtext==0.9.0
+#pip install transformers
+#pip install git+https://github.com/PyTorchLightning/pytorch-lightning fsspec --no-deps --target=$nb_path
+#pip install wandb --quiet
+
+#------------------------------------Imports------------------------------------
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+import torch
+import torchtext
+from torchtext import data
+from torchtext.legacy.data import Field, TabularDataset, BucketIterator, Iterator
+from transformers import RobertaTokenizer, RobertaModel, AdamW, get_linear_schedule_with_warmup, AutoTokenizer, AutoModelForMaskedLM
+
+import wandb
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import logging
+logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
+
+from utiliyFunctions import *
+from modelFunctions import *
+
+#-------------------------------------Paths-------------------------------------
+
+data_path = '/dataset/toxTwentyCast.csv'
+output_path = '/outputs/'
+
+#-------------------------------------Dataset Load------------------------------
+
+df = pd.read_csv(data_path)
+
+#-------------------------------------Wandb login-------------------------------
+
+wandb.login()
+
+#-------------------------------------Tokenizer definition----------------------
+
+BERT_MODEL_NAME = 'seyonec/BPE_SELFIES_PubChem_shard00_120k'
+tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL_NAME)
+
+MAX_SEQ_LEN = 256
+BATCH_SIZE = 16
+
+PAD_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
+UNK_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.unk_token)
+
+#-------------------------------------Dataloaders-------------------------------
+
+label_field = torchtext.legacy.data.Field(sequential=False, use_vocab=False, batch_first=True)
+text_field = torchtext.legacy.data.Field(use_vocab=False,
+                   tokenize=tokenizer.encode,
+                   include_lengths=False,
+                   batch_first=True,
+                   fix_length=MAX_SEQ_LEN,
+                   pad_token=PAD_INDEX,
+                   )
+
+fields = {'text' : ('text', text_field), 'labels' : ('labels', label_field)}
+
+train_data, valid_data, test_data = torchtext.legacy.data.TabularDataset(data_path,
+                                                   format='CSV',
+                                                   fields=fields,
+                                                   skip_header=False).split(split_ratio=[0.70, 0.2, 0.1],
+                                                                            stratified=True,
+                                                                            strata_field='labels')
+
+train_iter, valid_iter = BucketIterator.splits((train_data, valid_data),
+                                               batch_size=BATCH_SIZE,
+                                               shuffle=True,
+                                               sort_key=lambda x: len(x.text),
+                                               sort=True,
+                                               sort_within_batch=False)
+
+test_iter = Iterator(test_data, batch_size=BATCH_SIZE, train=False, shuffle=False, sort=False)
+
+#-------------------------------------Main training loop------------------------
+NUM_EPOCHS = 3
+steps_per_epoch = len(train_iter)
+
+model = ROBERTAClassifier()
+
+
+optimizer = AdamW(model.parameters(), lr=1e-4)
+scheduler = get_linear_schedule_with_warmup(optimizer,
+                                            num_warmup_steps=steps_per_epoch*1,
+                                            num_training_steps=steps_per_epoch*NUM_EPOCHS)
+
+print("======================= Start pretraining ==============================")
+
+pretrain(model=model,
+         train_iter=train_iter,
+         valid_iter=valid_iter,
+         optimizer=optimizer,
+         scheduler=scheduler,
+         num_epochs=NUM_EPOCHS,
+         valid_period=927)
+
+NUM_EPOCHS = 6
+print("======================= Start training =================================")
+optimizer = AdamW(model.parameters(), lr=2e-6)
+scheduler = get_linear_schedule_with_warmup(optimizer,
+                                            num_warmup_steps=steps_per_epoch*2,
+                                            num_training_steps=steps_per_epoch*NUM_EPOCHS)
+
+train(model=model,
+      train_iter=train_iter,
+      valid_iter=valid_iter,
+      optimizer=optimizer,
+      scheduler=scheduler,
+      num_epochs=NUM_EPOCHS,
+         valid_period=927)
+
+model = ROBERTAClassifier()
+model = model.to(device)
+
+load_checkpoint(output_path + '/model.pkl', model)
+
+evaluate(model, test_iter)
+
+print(len(train_data))
+print(len(valid_data))
+print(len(test_data))
---
+++ modelFunctions.py
@@ -0,0 +1,239 @@
+#------------------------------------Imports------------------------------------
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+import torch
+import torchtext
+from torchtext import data
+from torchtext.legacy.data import Field, TabularDataset, BucketIterator, Iterator
+from transformers import RobertaTokenizer, RobertaModel, AdamW, get_linear_schedule_with_warmup, AutoTokenizer, AutoModelForMaskedLM
+
+import wandb
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import logging
+logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
+
+from utiliyFunctions import *
+#------------------------------------Pretrain-----------------------------------
+
+def pretrain(model,
+             optimizer,
+             train_iter,
+             valid_iter,
+             scheduler = None,
+             valid_period = len(train_iter),
+             num_epochs = 1):
+
+    # Pretrain linear layers, do not train bert
+    wandb.init(project="newTestPretrain")
+    for param in model.roberta.parameters():
+        param.requires_grad = False
+
+    model.train()
+
+    # Initialize losses and loss histories
+    train_loss = 0.0
+    valid_loss = 0.0
+    global_step = 0
+
+    # Train loop
+    for epoch in range(num_epochs):
+        for (source, target), _ in train_iter:
+            mask = (source != PAD_INDEX).type(torch.uint8)
+
+            y_pred = model(input_ids=source,
+                           attention_mask=mask)
+
+            loss = torch.nn.CrossEntropyLoss()(y_pred, target)
+
+            loss.backward()
+
+            # Optimizer and scheduler step
+            optimizer.step()
+            scheduler.step()
+
+            optimizer.zero_grad()
+
+            # Update train loss and global step
+            train_loss += loss.item()
+            global_step += 1
+
+            # Validation loop. Save progress and evaluate model performance.
+            print('a')
+            print(global_step, valid_period)
+
+            if global_step % valid_period == 0:
+                model.eval()
+                print(len(valid_iter))
+                with torch.no_grad():
+                    for (source, target), _ in valid_iter:
+                        print(source, target)
+                        mask = (source != PAD_INDEX).type(torch.uint8)
+
+                        y_pred = model(input_ids=source,
+                                       attention_mask=mask)
+
+                        loss = torch.nn.CrossEntropyLoss()(y_pred, target)
+
+                        valid_loss += loss.item()
+
+                # Store train and validation loss history
+                train_loss = train_loss / valid_period
+                valid_loss = valid_loss / len(valid_iter)
+
+                model.train()
+
+                # print summary
+                wandb.log({'epoch': epoch, 'global_step': global_step, 'train_loss': train_loss, 'valid_loss': valid_loss})
+                print('Epoch [{}/{}], global step [{}/{}], PT Loss: {:.4f}, Val Loss: {:.4f}'
+                      .format(epoch+1, num_epochs, global_step, num_epochs*len(train_iter),
+                              train_loss, valid_loss))
+
+                train_loss = 0.0
+                valid_loss = 0.0
+
+    # Set bert parameters back to trainable
+    for param in model.roberta.parameters():
+        param.requires_grad = True
+
+    wandb.finish()
+    print('Pre-training done!')
+
+#------------------------------------Train--------------------------------------
+
+def train(model,
+          optimizer,
+          train_iter,
+          valid_iter,
+          scheduler = None,
+          num_epochs = 5,
+          valid_period = len(train_iter),
+          output_path = output_path):
+
+    # Initialize losses and loss histories
+    wandb.init(project="newTestTrain")
+
+    train_loss = 0.0
+    valid_loss = 0.0
+    train_loss_list = []
+    valid_loss_list = []
+    best_valid_loss = float('Inf')
+
+    global_step = 0
+    global_steps_list = []
+
+    model.train()
+
+    # Train loop
+    for epoch in range(num_epochs):
+        print(epoch)
+        for (source, target), _ in train_iter:
+            mask = (source != PAD_INDEX).type(torch.uint8)
+
+            y_pred = model(input_ids=source,
+                           attention_mask=mask)
+            #output = model(input_ids=source,
+            #              labels=target,
+            #              attention_mask=mask)
+
+            loss = torch.nn.CrossEntropyLoss()(y_pred, target)
+            #loss = output[0]
+
+            loss.backward()
+
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+
+            # Optimizer and scheduler step
+            optimizer.step()
+            scheduler.step()
+
+            optimizer.zero_grad()
+
+            # Update train loss and global step
+            train_loss += loss.item()
+            global_step += 1
+
+            # Validation loop. Save progress and evaluate model performance.
+            if global_step % valid_period == 0:
+                model.eval()
+
+                with torch.no_grad():
+                    for (source, target), _ in valid_iter:
+                        mask = (source != PAD_INDEX).type(torch.uint8)
+
+                        y_pred = model(input_ids=source,
+                                       attention_mask=mask)
+                        #output = model(input_ids=source,
+                        #               labels=target,
+                        #               attention_mask=mask)
+
+                        loss = torch.nn.CrossEntropyLoss()(y_pred, target)
+                        #loss = output[0]
+
+                        valid_loss += loss.item()
+
+                # Store train and validation loss history
+                train_loss = train_loss / valid_period
+                valid_loss = valid_loss / len(valid_iter)
+                train_loss_list.append(train_loss)
+                valid_loss_list.append(valid_loss)
+                global_steps_list.append(global_step)
+
+                # print summary
+                wandb.log({'epoch': epoch, 'global_step': global_step, 'train_loss': train_loss, 'valid_loss': valid_loss})
+                print('Epoch [{}/{}], global step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}'
+                      .format(epoch+1, num_epochs, global_step, num_epochs*len(train_iter),
+                              train_loss, valid_loss))
+
+                # checkpoint
+                if best_valid_loss > valid_loss:
+                    best_valid_loss = valid_loss
+                    save_checkpoint(output_path + '/model.pkl', model, best_valid_loss)
+                    save_metrics(output_path + '/metric.pkl', train_loss_list, valid_loss_list, global_steps_list)
+
+                train_loss = 0.0
+                valid_loss = 0.0
+                model.train()
+
+    wandb.finish()
+    save_metrics(output_path + '/metric.pkl', train_loss_list, valid_loss_list, global_steps_list)
+    print('Training done!')
+
+#------------------------------------Evaluation---------------------------------
+
+def evaluate(model, test_loader):
+    y_pred = []
+    y_true = []
+
+    model.eval()
+    with torch.no_grad():
+        for (source, target), _ in test_loader:
+                mask = (source != PAD_INDEX).type(torch.uint8)
+
+                output = model(source, attention_mask=mask)
+
+                y_pred.extend(torch.argmax(output, axis=-1).tolist())
+                y_true.extend(target.tolist())
+
+    print('Classification Report:')
+    print(classification_report(y_true, y_pred, labels=[1,0], digits=4))
+
+    cm = confusion_matrix(y_true, y_pred, labels=[1,0])
+    ax = plt.subplot()
+
+    sns.heatmap(cm, annot=True, ax = ax, cmap='Blues', fmt="d")
+
+    ax.set_title('Confusion Matrix')
+
+    ax.set_xlabel('Predicted Labels')
+    ax.set_ylabel('True Labels')
+
+    ax.xaxis.set_ticklabels(['FAKE', 'REAL'])
+    ax.yaxis.set_ticklabels(['FAKE', 'REAL'])
