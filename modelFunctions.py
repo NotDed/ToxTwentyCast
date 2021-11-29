04114dbdf626857ef271e0ca6963e29e91d3bb74
daniel <Daniel.serna.olarte@gmail.com>
2021-11-29 17:23:31 -0500

    WIP

--- modelFunctions.py
+++ modelFunctions.py
@@ -83,6 +83,8 @@ def pretrain(model,
             y_pred = model(input_ids=source,
                            attention_mask=mask)
 
+            print('target: ',target)
+            print('y_pred: ',y_pred)
             print('source: ',source.shape, ' target: ',target.shape, ' y_pred: ',y_pred.shape)
 
             loss = torch.nn.CrossEntropyLoss()(y_pred, target)
@@ -176,7 +178,8 @@ def train(model,
                            attention_mask=mask)
 
 
-
+            print('target: ',target)
+            print('y_pred: ',y_pred)
             print('source: ',source.shape(), ' target: ',target.shape(), ' y_pred: ',y_pred.shape())
             #output = model(input_ids=source,
             #              labels=target,
