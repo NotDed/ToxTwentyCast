b0606cf45fc3ca65487dd6fd39e115b3114d9659
daniel <Daniel.serna.olarte@gmail.com>
2021-12-22 02:14:47 -0500

    wip

--- utilityFunctions.py
+++ utilityFunctions.py
@@ -2,13 +2,13 @@ import torch
 from transformers import RobertaTokenizer, RobertaModel, AdamW, get_linear_schedule_with_warmup, AutoTokenizer, AutoModelForMaskedLM
 
 def save_checkpoint(path, model, valid_loss):
-    torch.save({'model_state_dict': model.state_dict(),
+    torch.save({'model_state_dict': model.module.state_dict(),
                   'valid_loss': valid_loss}, path)
 
 
 def load_checkpoint(path, model):
     state_dict = torch.load(path)
-    model.load_state_dict(state_dict['model_state_dict'])
+    model.module.load_state_dict(state_dict['model_state_dict'])
 
     return state_dict['valid_loss']
 
