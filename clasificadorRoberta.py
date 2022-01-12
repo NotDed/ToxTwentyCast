6152aa65608cc21d7fbafe7741d1939eb348646d
juanmunera <pablomunera2@utp.edu.co>
2022-01-12 21:56:44 +0100

    wip

--- clasificadorRoberta.py
+++ clasificadorRoberta.py
@@ -26,13 +26,13 @@ import utilityFunctions
 class ROBERTAClassifier(torch.nn.Module):
     def __init__(self, BERT_MODEL_NAME):
         super(ROBERTAClassifier, self).__init__()
-        self.l1 = RobertaModel.from_pretrained(BERT_MODEL_NAME, return_dict=False)
+        self.roberta = RobertaModel.from_pretrained(BERT_MODEL_NAME, return_dict=False)
         self.pre_classifier = torch.nn.Linear(768, 768)
         self.dropout = torch.nn.Dropout(0.3)
         self.classifier = torch.nn.Linear(768, 1)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
-        output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        output_1 = self.roberta(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         hidden_state = output_1[0]
         pooler = hidden_state[:, 0]
         pooler = self.pre_classifier(pooler)
