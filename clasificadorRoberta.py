5b75191973539380b167da4415f5deb1c33d3cb0
juanmunera <pablomunera2@utp.edu.co>
2022-01-12 22:00:34 +0100

    wip

--- clasificadorRoberta.py
+++ clasificadorRoberta.py
@@ -31,7 +31,7 @@ class ROBERTAClassifier(torch.nn.Module):
         self.dropout = torch.nn.Dropout(0.3)
         self.classifier = torch.nn.Linear(768, 1)
 
-    def forward(self, input_ids, attention_mask, token_type_ids):
+    def forward(self, input_ids, attention_mask):
         output_1 = self.roberta(input_ids=input_ids, attention_mask=attention_mask)
         hidden_state = output_1[0]
         pooler = hidden_state[:, 0]
