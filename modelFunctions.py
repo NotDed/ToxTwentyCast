5f221f38a3e3c499f15099954911b39c1435f1df
juanmunera <pablomunera2@utp.edu.co>
2022-01-12 16:00:16 +0100

    optuna

--- modelFunctions.py
+++ modelFunctions.py
@@ -211,7 +211,7 @@ def train(model,
 
     # Initialize losses and loss histories
     for param in model.module.roberta.parameters():
-       param.requires_grad = True
+       param.requires_grad = False
 
     train_loss = 0.0
     valid_loss = 0.0
