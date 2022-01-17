a7aadb1e58d99754c046de3f090f79a114497437
juanmunera <pablomunera2@utp.edu.co>
2022-01-17 18:19:04 +0100

    wip

--- source/config.py
+++ source/config.py
@@ -1,11 +1,11 @@
 import transformers
-from transformers import  Tokenizer
+from transformers import  AutoTokenizer
 MAX_LEN = 258
 TRAIN_BATCH_SIZE = 16
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
-BERT_PATH = "../input/roberta_model"
-MODEL_PATH = "model.bin"
+BERT_PATH = "BPE_SELFIES_PubChem_shard00_160k"
+MODEL_PATH = "BPE_SELFIES_PubChem_shard00_160k/pytorch_model.bin"
 TRAINING_FILE = "../input/toxTwentyCast.csv"
 TOKENIZER = AutoTokenizer.from_pretrained(
     BERT_PATH,
