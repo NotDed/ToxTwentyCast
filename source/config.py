a7dc9a313fbad4b9fdbe4cdfeb9a9e7bf1656a88
juanmunera <pablomunera2@utp.edu.co>
2022-01-17 18:24:54 +0100

    wip

--- source/config.py
+++ source/config.py
@@ -4,8 +4,8 @@ MAX_LEN = 258
 TRAIN_BATCH_SIZE = 16
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
-BERT_PATH = "BPE_SELFIES_PubChem_shard00_160k"
-MODEL_PATH = "BPE_SELFIES_PubChem_shard00_160k/pytorch_model.bin"
+BERT_PATH = "https://huggingface.co/seyonec/BPE_SELFIES_PubChem_shard00_160k/tree/main"
+MODEL_PATH = "https://huggingface.co/seyonec/BPE_SELFIES_PubChem_shard00_160k/blob/main/pytorch_model.bin"
 TRAINING_FILE = "../input/toxTwentyCast.csv"
 TOKENIZER = AutoTokenizer.from_pretrained(
     BERT_PATH,
