e850e33abb69f584d3a47cfef53a81025e35b8fd
juanmunera <pablomunera2@utp.edu.co>
2022-01-18 00:21:44 +0100

    wip

--- source/dataset.py
+++ source/dataset.py
@@ -22,22 +22,23 @@ class BERTDataset:
             add_special_tokens = True,
             max_length = self.max_len,
             pad_to_max_length = True,
+            return_token_type_ids = True
         )
         
         ids=inputs["input_ids"]
         mask = inputs["attention_mask"]
-        #token_type_ids = inputs["token_type_ids"]
+        token_type_ids = inputs["token_type_ids"]
 
 
         padding_length = self.max_len - len(ids)
         ids = ids + ([0] * padding_length)
         mask = mask + ([0] * padding_length)
-        #token_type_ids = token_type_ids + ([0]* padding_length)
+        token_type_ids = token_type_ids + ([0]* padding_length)
 
         return{
             'ids': torch.tensor(ids, dtype=torch.long),
             'mask': torch.tensor(mask, dtype=torch.long),
-            #'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
             'targets': torch.tensor(self.labels[item], dtype=torch.float),
 
         }
