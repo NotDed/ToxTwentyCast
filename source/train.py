cdc59a05346b3f026a61aefb55510bb22c625824
daniel <Daniel.serna.olarte@gmail.com>
2022-01-17 15:02:57 -0500

    wip

--- source/train.py
+++ source/train.py
@@ -35,7 +35,7 @@ def run():
         labels = df_train.labels.values
     )
 
-    train_data_loader = torch.utils.data.dataloader = torch.utils.DataLoader(
+    train_data_loader = torch.utils.data.Dataloader(
         train_dataset, 
         batch_size=config.TRAIN_BATCH_SIZE, 
         num_workers = 4
