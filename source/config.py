3c3dc6b2dfd6b8c2c6cf2e5281452161fc87c87d
daniel <Daniel.serna.olarte@gmail.com>
2022-01-17 13:11:17 -0500

    wip

--- source/config.py
+++ source/config.py
@@ -5,7 +5,7 @@ TRAIN_BATCH_SIZE = 16
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
 BERT_PATH = "https://huggingface.co/seyonec/BPE_SELFIES_PubChem_shard00_160k/tree/main"
-MODEL_PATH = "https://huggingface.co/seyonec/BPE_SELFIES_PubChem_shard00_160k/blob/main/pytorch_model.bin"
+# MODEL_PATH = "pytorch_model.bin"
 TRAINING_FILE = "../input/toxTwentyCast.csv"
 TOKENIZER = AutoTokenizer.from_pretrained(
     BERT_PATH,
