1e5fc23750436dcd2418ab412d83f123bd3d0ba4
daniel <Daniel.serna.olarte@gmail.com>
2022-01-30 18:38:23 -0500

    wip

---
+++ modelFunctions.py
@@ -0,0 +1,83 @@
+import torch
+from tqdm import tqdm
+
+from torch import cuda
+device = 'cuda' if cuda.is_available() else 'cpu'
+
+
+def calcuate_accuracy(preds, targets):
+    n_correct = (preds==targets).sum().item()
+    return n_correct
+
+# Defining the training function on the 80% of the dataset for tuning the distilbert model
+
+def train(epoch, model, training_loader, loss_function, optimizer):
+    tr_loss = 0
+    n_correct = 0
+    nb_tr_steps = 0
+    nb_tr_examples = 0
+    model.train()
+    for _,data in tqdm(enumerate(training_loader, 0)):
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
+        targets = data['targets'].to(device, dtype = torch.long)
+
+        outputs = model(ids, mask, token_type_ids)
+        loss = loss_function(outputs, targets)
+        tr_loss += loss.item()
+        big_val, big_idx = torch.max(outputs.data, dim=1)
+        n_correct += calcuate_accuracy(big_idx, targets)
+
+        nb_tr_steps += 1
+        nb_tr_examples+=targets.size(0)
+        
+        if _%5000==0:
+            loss_step = tr_loss/nb_tr_steps
+            accu_step = (n_correct*100)/nb_tr_examples 
+            print(f"Training Loss per 5000 steps: {loss_step}")
+            print(f"Training Accuracy per 5000 steps: {accu_step}")
+
+        optimizer.zero_grad()
+        loss.backward()
+        # # When using GPU
+        optimizer.step()
+
+    print(f'The Total Accuracy for Epoch {epoch}: {(n_correct*100)/nb_tr_examples}')
+    epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    print(f"Training Loss Epoch: {epoch_loss}")
+    print(f"Training Accuracy Epoch: {epoch_accu}")
+
+    return model, epoch_accu
+
+
+def valid(model, testing_loader, loss_function):
+    model.eval()
+    n_correct = 0; n_wrong = 0; total = 0; tr_loss=0; nb_tr_steps=0; nb_tr_examples=0
+    with torch.no_grad():
+        for _, data in tqdm(enumerate(testing_loader, 0)):
+            ids = data['ids'].to(device, dtype = torch.long)
+            mask = data['mask'].to(device, dtype = torch.long)
+            token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+            targets = data['targets'].to(device, dtype = torch.long)
+            outputs = model(ids, mask, token_type_ids).squeeze()
+            loss = loss_function(outputs, targets)
+            tr_loss += loss.item()
+            big_val, big_idx = torch.max(outputs.data, dim=1)
+            n_correct += calcuate_accuracy(big_idx, targets)
+
+            nb_tr_steps += 1
+            nb_tr_examples+=targets.size(0)
+            
+            if _%5000==0:
+                loss_step = tr_loss/nb_tr_steps
+                accu_step = (n_correct*100)/nb_tr_examples
+                print(f"Validation Loss per 100 steps: {loss_step}")
+                print(f"Validation Accuracy per 100 steps: {accu_step}")
+    epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    print(f"Validation Loss Epoch: {epoch_loss}")
+    print(f"Validation Accuracy Epoch: {epoch_accu}")
+    
+    return epoch_accu
