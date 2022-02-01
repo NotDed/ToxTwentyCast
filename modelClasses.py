f70cf4e5374dc026cb29ad968473a9cca8b3c70a
juanmunera <pablomunera2@utp.edu.co>
2022-02-01 13:36:26 +0100

    wip

--- modelClasses.py
+++ modelClasses.py
@@ -28,7 +28,7 @@ class SentimentData(Dataset):
             None,
             add_special_tokens=True,
             max_length=self.max_len,
-            padding=True,
+            padding= self.max_len,
             return_token_type_ids=True
         )
         ids = inputs['input_ids']
