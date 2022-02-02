468f8914b4e83476960d8ad9f76c08144f433dfb
daniel <Daniel.serna.olarte@gmail.com>
2022-02-01 19:10:04 -0500

    wip

--- TTC3.py
+++ TTC3.py
@@ -62,7 +62,7 @@ model.to(device)
 loss_function = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
 
-EPOCHS = 1
+EPOCHS = 5
 
 #-------------------------------------Wandb login-------------------------------
 wandb.login()
