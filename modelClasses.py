9bbf4492770c507463febfe292e71bf01b8563be
juanmunera <pablomunera2@utp.edu.co>
2022-05-19 23:10:08 +0200

    wip

--- modelClasses.py
+++ modelClasses.py
@@ -74,7 +74,7 @@ class RobertaClass(torch.nn.Module):
         hidden_state =output_1[0]
         pooler = hidden_state[:, 0]
         pooler = self.pre_classifier(pooler)
-        pooler = torch.nn.ReLU()(pooler)
+        pooler = torch.nn.Sigmoid()(pooler)
         pooler = self.dropout(pooler)
         output = self.classifier(pooler)
         #output = self.threshold(output)
