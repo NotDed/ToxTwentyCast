5a835abe08d32662c0c632b1f20aef6321aed3d3
juanmunera <pablomunera2@utp.edu.co>
2022-05-20 23:38:42 +0200

    wip

--- modelClasses.py
+++ modelClasses.py
@@ -78,7 +78,7 @@ class RobertaClass(torch.nn.Module):
         pooler = self.dropout(pooler)
         output = self.classifier(pooler)
         output = torch.nn.LogSoftmax(output)
-        output = self.threshold(output)
+        #output = self.threshold(output)
         # hidden_state = output_1[0]
 
         # pooler = hidden_state[:, 0]
