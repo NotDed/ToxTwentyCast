f7c73198643e2ddd976c52d307b649cd87ade71a
juanmunera <pablomunera2@utp.edu.co>
2022-05-20 23:37:20 +0200

    wip

--- modelClasses.py
+++ modelClasses.py
@@ -56,7 +56,7 @@ class RobertaClass(torch.nn.Module):
         self.pre_classifier = torch.nn.Linear(768, 64)
         self.dropout = torch.nn.Dropout(0.3)
         self.classifier = torch.nn.Linear(64, 1)
-        self.threshold = torch.nn.Threshold(0.4 , 1, 0)
+        #self.threshold = torch.nn.Threshold(0.4 , 1, 0)
         
         # self.pre_classifier = torch.nn.Linear(768, 768)
         # self.norm1 = torch.nn.LayerNorm(768)
@@ -77,7 +77,7 @@ class RobertaClass(torch.nn.Module):
         pooler = torch.nn.Sigmoid()(pooler)
         pooler = self.dropout(pooler)
         output = self.classifier(pooler)
-        output = torch.nn.Sigmoid()(output)
+        output = torch.nn.LogSoftmax(output)
         output = self.threshold(output)
         # hidden_state = output_1[0]
 
