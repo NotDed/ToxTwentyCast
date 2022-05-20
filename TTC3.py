a60aac16c586828ebbfbf9285c72899f339c3a36
juanmunera <pablomunera2@utp.edu.co>
2022-05-20 23:25:28 +0200

    wip

--- TTC3.py
+++ TTC3.py
@@ -20,8 +20,8 @@ from modelClasses import SentimentData, RobertaClass
 from modelFunctions import train, valid
 
 #new_df = pd.read_csv('~/ToxTwentyCast/dataset/toxTwentyCast.csv')
-#new_df = pd.read_csv('~/ToxTwentyCast/dataset/NR.csv')
-new_df = pd.read_csv('~/ToxTwentyCast/dataset/Tox21.csv')
+new_df = pd.read_csv('~/ToxTwentyCast/dataset/Datasets NR/NR-AhR.csv')
+#new_df = pd.read_csv('~/ToxTwentyCast/dataset/Tox21.csv')
 
 # Defining some key variables that will be used later on in the training
 def mainTrain():
