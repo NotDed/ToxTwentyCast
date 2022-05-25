8adac33c8095bfbfe423cec2678bd084c39d94db
juanmunera <pablomunera2@utp.edu.co>
2022-05-25 17:26:47 +0200

    wip

--- modelFunctions.py
+++ modelFunctions.py
@@ -47,7 +47,7 @@ def train(epoch, model, training_loader, loss_function, optimizer):
         print('c')
         print(outputs)
         print('b')
-        loss = loss_function(outputs.totorch.FloatTensor(list()), targets.tolist())
+        loss = loss_function(torch.FloatTensor(outputs.tolist()), targets.tolist())
         #loss = loss_function(outputs.tolist(), targets.tolist())
         #print(outputs)
         predictions.extend(outputs)
