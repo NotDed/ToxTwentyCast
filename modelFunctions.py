c7af14ba1524d028f563226a4f8de4b925abdd0c
juanmunera <pablomunera2@utp.edu.co>
2022-06-01 00:26:02 +0200

    wip

--- modelFunctions.py
+++ modelFunctions.py
@@ -101,8 +101,8 @@ def valid(model, loader, loss_function):
     wandb.log({'Accuracy': accuracy1})
 
     outputs = np.asarray([1 if i else 0 for i in (np.asarray(y_pred) >= 0.5)])
-    
-    return auc_k, precision, f1, y_pred, loss.item()
+    return roc_auc_score(y_target, y_pred), average_precision_score(y_target, y_pred), f1_score(y_target, outputs), y_pred, loss.item()
+    #return auc_k, precision, f1, y_pred, loss.item()
   
   
 
