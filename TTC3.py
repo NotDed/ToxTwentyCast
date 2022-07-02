5666d48631313b0dae6982c5d86c972d1bd6b45f
juanmunera <pablomunera2@utp.edu.co>
2022-07-02 20:54:29 +0200

    wip

--- TTC3.py
+++ TTC3.py
@@ -95,6 +95,7 @@ def mainTrain():
     with torch.set_grad_enabled(False):
         auc, auprc, f1, predictions, loss, y_pred, y_target= valid(model, testing_loader, loss_function)
         predicciones[str(epoch)] = [y_pred, y_target]
+        print(len(predicciones))
         print('Validation at Epoch {}, AUROC: {}, AUPRC: {}, F1: {}, LOSS: {}'.format(epoch + 1, auc, auprc, f1, loss))
     
     print('predictions by epoch')
