f08277a8aa5ea64dd957a2f59f14d0e838919167
daniel <Daniel.serna.olarte@gmail.com>
2022-07-25 00:04:48 -0500

    wip

--- Api/Front/index.js
+++ Api/Front/index.js
@@ -13,13 +13,15 @@ function askToxicity(){
         for (var key in data){
             console.log(data[key]);
             var p = document.createElement("p");
-            p.classList = "mx-auto p-1 m-1 w-1/2 rounded-full text-center text-white font-bold w-full";
+            p.classList = "mx-auto p-1 rounded-full text-center text-white font-bold w-full";
             if (data[key]['pred'] == 1){
                 p.classList.add('bg-red-500')
+                p.innerHTML = `${key} is toxic`;
             }else{
                 p.classList.add('bg-green-500')
+                p.innerHTML = `${key}  is not toxic`;
             }
-            p.innerHTML = data[key]['selfie'] +':'+ data[key]['pred'];
+            
             ul.appendChild(p);
         }
         console.log(data);
@@ -27,6 +29,6 @@ function askToxicity(){
       })
 }
 
-document.querySelector("#selfieInput").addEventListener("change", askToxicity);
-// document.querySelector("#predict").addEventListener("click", askToxicity);
+// document.querySelector("#selfieInput").addEventListener("change", askToxicity);
+document.querySelector("#predict").addEventListener("click", askToxicity);
 
