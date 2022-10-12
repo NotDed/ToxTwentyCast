924e35844374a686c3b2624d13ab4284828c6316
daniel <Daniel.serna.olarte@gmail.com>
2022-10-12 10:38:11 -0500

    wip

--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/predict";
+const API_URL = "https://0bb6-18-221-65-130.ngrok.io/predict";
 
 const tableStyles = "border-collapse table-auto w-full";
 const cellStyles = "mx-auto my-2 rounded-lg text-center font-bold w-full";
